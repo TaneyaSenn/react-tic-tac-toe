fd8b0fa99923d207aa1eb8eec255a37dd3670005
Taneya <twil609@aucklanduni.ac.nz>
2022-04-20 15:03:47 +1000

    handle clicks

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import { ReactDOM } from 'react';
+import React from 'react'
+import ReactDOM from 'react'
 import './index.css'
 
-
-class Square extends React.Component {
-    render() {
+function Square(props){
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]} onclick={() => this.handleClick(i)}/>;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
